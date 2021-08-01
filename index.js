9e11f11fa0cbc7f23ad1aed74630d9a63f667775
Amna Syeda <amna.syeda98@gmail.com>
2021-08-01 13:08:41 -0400

    post file created

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const User = require('./User');
+const Post = require('./Post');
 
 router.use('/users', userRoutes);
 
