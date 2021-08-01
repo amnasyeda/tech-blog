5446faf612d7f54399d5558e412547207ad6f80c
Amna Syeda <amna.syeda98@gmail.com>
2021-08-01 12:50:08 -0400

    login routes

--- User.js
+++ User.js
@@ -3,3 +3,9 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 class User extends Model {}
+checkPassword(loginPW) {
+    return bcrypt.compareSync(loginPW, this.password);
+}
+};
+
+User.init(
--- user-routes.js
+++ user-routes.js
@@ -31,17 +31,26 @@ router.get('/:id', (req, res) => {
         });
 });
 
-router.post('/', (req, res) => {
-    User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
 });
 
 router.put('/:id', (req, res) => {
