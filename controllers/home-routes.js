a4c2c92674daa29a479221ae4a9534e58c59f75a
Amna Syeda <amna.syeda98@gmail.com>
2021-08-02 21:46:03 -0400

    starter code for homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  res.render('homepage', {
+    id: 1,
+    content: 'Hi, my name is Amna.',
+    title: 'Name',
+    created_at: new Date(),
+    comments: [{}, {}],
+    user: {
+        username: 'amna'
+    }
+  });
 });
+
 
 module.exports = router;
