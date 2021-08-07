39511b6a6ff67e1e0685f6bd7b4ebd73d29d0bb4
Amna Syeda <amna.syeda98@gmail.com>
2021-08-06 21:11:01 -0400

    fixed single post

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const path = require('path');
-const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
 
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// hiding password
-require('dotenv').config();
-//
-
-const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -26,8 +23,6 @@ const sess = {
 };
 
 app.use(session(sess));
-
-const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
